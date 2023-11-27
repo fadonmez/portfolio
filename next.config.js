c8374e3d40e59ec7a380449724e8dae509fa2ee8
Fatih Donmez <fatihdonmezbusiness@outlook.com>
2023-11-27 19:48:35 +0300

    bug fix

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn-icons-png.flaticon.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'static-00.iconduck.com',
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
