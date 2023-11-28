80c22bc3d8be8746e12566ef61ed213d1f0f0dc5
Fatih Donmez <fatihdonmezbusiness@outlook.com>
2023-11-28 22:46:50 +0300

    color change

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,18 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className='flex-1 flex bg-black '>
+    <div className='flex-1 flex '>
       <MotionDiv
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.1, ease: 'easeInOut' }}
         className='container flex flex-col items-center justify-center '
       >
-        <h1 className='text-4xl md:text-6xl font-semibold text-center text-white '>
+        <h1 className='text-4xl md:text-6xl font-semibold text-center  '>
           Welcome to my <br />
           <span className=' text-green-500'>portfolio and personal blog !</span>
         </h1>
-        <p className='text-zinc-200 text-center mt-6 text-md md:text-lg'>
+        <p className='text-zinc-600 text-center mt-6 text-md md:text-lg'>
           I&apos;m a software engineer with a passion for creating beautiful and
           functional web applications. I&apos;m currently working at a startup
           called{' '}
